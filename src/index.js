4689ff6341d1dedc6624581408f90a2852fccb6e
Denis <ds19092004@gmail.com>
2023-08-19 11:19:21 +0200

    feat: do to reable func -(Sat, Aug 19, 2023 11:19:20 AM)-

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const digits = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    return number
+        .toString()
+        .split("")
+        .map((item) => {
+            for (let index = 0; index < digits.length; index++) {
+                const element = digits[index];
+                if (item == index) {
+                    item = element;
+                    return item;
+                }
+            }
+        })
+        .join(" ");
+};
