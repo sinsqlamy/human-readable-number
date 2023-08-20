cfb4f88b21814fcb1cf4500fcdb408c6b518da00
Denis <ds19092004@gmail.com>
2023-08-20 12:14:07 +0200

    fix: reable to func with new approach -(Sun, Aug 20, 2023 12:14:06 PM)-

--- src/index.js
+++ src/index.js
@@ -1,98 +1,46 @@
-const { constants } = require("mocha/lib/suite");
+const digits = [
+    "zero", "one", "two", "three", "four", 
+    "five", "six", "seven", "eight", "nine"
+];
+
+const teens = [
+    "ten", "eleven", "twelve", "thirteen", 
+    "fourteen", "fifteen", "sixteen", "seventeen", 
+    "eighteen", "nineteen"
+];
+
+const tens = [
+    "", "", "twenty", "thirty", "forty", 
+    "fifty", "sixty", "seventy", "eighty", "ninety"
+];
 
 module.exports = function toReadable(number) {
-    const digits = [
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-    ];
-
-    let reablePower = number.toString().split("").length;
-
-    let reable = number
-        .toString()
-        .split("")
-        .map((item) => {
-            for (let index = 0; index < digits.length; index++) {
-                const element = digits[index];
-
-                if (item == index) {
-                    item = element;
-                    return item;
-                }
-            }
-        });
-
-    if (reablePower == 3) {
-        reable[0] += " hundred";
-
-        if (reable[1][reable[1].length - 1] == "t") {
-            reable[1] += "y";
-        } else {
-            reable[1] += "ty";
-        }
+    if (number === 0) {
+        return "zero";
     }
-    if (reablePower == 2) {
-        if (reable[0][reable[0].length - 1] == "t") {
-            reable[0] += "y";
+
+    function convertBelow100(num) {
+        if (num < 10) {
+            return digits[num];
+        } else if (num < 20) {
+            return teens[num - 10];
         } else {
-            reable[0] += "ty";
+            const tenDigit = Math.floor(num / 10);
+            const unitDigit = num % 10;
+            return `${tens[tenDigit]}${unitDigit !== 0 ? " " + digits[unitDigit] : ""}`;
         }
     }
 
-    reable.forEach((element) => {
-        if (element == "zero" && reablePower != 1) {
-            reable.pop();
-        }
-    });
+    if (number < 100) {
+        return convertBelow100(number);
+    }
 
-    reable.forEach((element) => {
-        if (element == "onety") {
-            if (reable[2] == undefined) {
-                return;
-            }
-            if (reable[2][reable[2].length - 1] == "t") reable[2] += "een";
-            else reable[2] += "teen";
-            reable.splice(1, 1);
-        }
-        if (element == "zeroty") {
-            reable.splice(1, 1);
-        }
-    });
-    reable = reable.map((item) => {
-        if (item == "fivety") {
-            return (item = "fifty");
-        } else if (item == "fourty") {
-            return (item = "forty");
-        } else if (item == "threety") {
-            return (item = "thirty");
-        } else if (item == "onety") {
-            return (item = "ten");
-        } else if (item == "twoty") {
-            return (item = "twenty");
-        } else if (item == "oneteen") {
-            return (item = "eleven");
-        } else if (item == "twoteen") {
-            return (item = "twelve");
-        } else if (item == "threeteen") {
-            return (item = "thirteen");
-        } else if (item == "fiveteen") {
-            return (item = "fifteen");
-        } else {
-            return (item = item);
-        }
-    });
+    const hundredDigit = Math.floor(number / 100);
+    const remainder = number % 100;
 
-    return reable.join(" ");
+    if (remainder === 0) {
+        return `${digits[hundredDigit]} hundred`;
+    }
+
+    return `${digits[hundredDigit]} hundred ${convertBelow100(remainder)}`;
 };
-
-/*
-
-*/
